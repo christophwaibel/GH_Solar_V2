5bb0c988dba151719887cb7f92c4b605d2f940bc
Christoph Waibel <christophwaibel@gmx.de>
2017-04-02 23:13:51 +0200

    Add files via upload

---
+++ Tester/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//using Rhino.Geometry;
+//using Rhino;
+
+using SolarModel;
+using System.Diagnostics;
+
+namespace Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Mesh mesh = new Mesh();
+            ////mesh.Vertices.Add(0.0, 0.0, 1.0);   //0
+            ////mesh.Vertices.Add(1.0, 0.0, 1.0);   //1
+            ////mesh.Vertices.Add(1.0, 1.0, 1.0);   //2
+            ////mesh.Vertices.Add(0.0, 1.0, 1.0);   //3
+
+            ////mesh.Faces.AddFace(0, 1, 2, 3);
+
+            //Ray3d ray = new Ray3d(new Point3d(0.5,0.5,0), new Vector3d(0,0,1));
+            //Point3d p = ray.PointAt(0.1);
+
+            ////double inters = Rhino.Geometry.Intersect.Intersection.MeshRay(mesh, ray);
+
+            ////Console.WriteLine(inters);
+            //Console.WriteLine("x: {0}, y: {1}, z: {2}", p.X, p.Y, p.Z);
+            //Console.ReadKey();
+
+            Console.WriteLine("hi");
+
+            //IcoSphere ico = new IcoSphere(0);
+            //List<int[]> test = ico.getFaces();
+            //List<double[]> coords = ico.getVertexCoordinates();
+
+            ////coords.RemoveAt(1);
+            
+
+
+            //Stopwatch watch = new Stopwatch();
+            //watch.Start();
+            ////SkyDome dome = new SkyDome(2, 2013, 47.3673,8.55 );        //!!!!!!!!!!!!!!!
+            //Console.WriteLine(Convert.ToInt32(false));
+            //Console.WriteLine("hi");
+            //watch.Stop();
+            //Console.WriteLine(watch.Elapsed.TotalMilliseconds);
+            //Console.ReadKey();
+
+
+
+
+            //load in weather file DHI
+            List<double> DHI = new List<double>();
+            List<double> DNI = new List<double>();
+            int counter = 0;
+            string line;
+
+            // Read the file and display it line by line.
+            System.IO.StreamReader file =
+               new System.IO.StreamReader("C:\\Users\\Chris\\Desktop\\DHI.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                DHI.Add(Convert.ToDouble(line));
+                //Console.WriteLine(line);
+                counter++;
+            }
+            file.Close();
+
+            System.IO.StreamReader file2 = new System.IO.StreamReader("C:\\Users\\Chris\\Desktop\\DNI.txt");
+            while ((line = file2.ReadLine()) != null)
+            {
+                DNI.Add(Convert.ToDouble(line));
+                //Console.WriteLine(line);
+                counter++;
+            }
+            file.Close();
+
+
+
+
+
+            //Sunvectors are always the same for the location and year
+            int recursion = 2;
+            double longitude = 8.5500025;
+            double latitude = 47.367347;
+            int year = 2016;
+
+
+            List<SunVector> sunvectors = new List<SunVector>();
+            for (int m = 1; m <= 12; m++)
+            {
+                int daysInMonth = System.DateTime.DaysInMonth(year, m);
+                for (int d = 1; d <= daysInMonth; d++)
+                {
+                    for (int i = 1; i <= 24; i++)
+                    {
+                        SunVector sunvec = new SunVector(year, m, d, i, 0, 0, longitude, latitude);
+                        sunvectors.Add(sunvec);
+                    }
+                }
+            }
+
+            Context.cWeatherdata weather;
+            weather.DHI = new List<double>();
+            weather.DNI = new List<double>();
+            for (int i = 0; i < 8760; i++)
+            {
+                weather.DHI.Add(DHI[i]);
+                weather.DNI.Add(DNI[i]);
+            }
+
+            Context.cLocation location;
+            location.dLatitude = latitude;
+            location.dLongitude = longitude;
+            location.dTgmt = 1;
+
+            Sensorpoint p = new Sensorpoint(year, weather, location, sunvectors,0,0,recursion);
+            p.CalcIrradiation();
+
+            for (int i = 0; i < p.I.Count(); i++)
+            {
+                Console.WriteLine(p.I[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
