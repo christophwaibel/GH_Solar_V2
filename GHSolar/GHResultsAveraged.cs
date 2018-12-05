946e62f0b04733a0496144de481fffe24cf3283c
Christoph Waibel <christophwaibel@gmx.de>
2018-12-05 15:36:45 +0100

    added output types, new for solar access

--- GHSolar/GHResultsAveraged.cs
+++ GHSolar/GHResultsAveraged.cs
@@ -26,6 +26,10 @@ namespace GHSolar
         {
             pManager.AddMeshParameter("Mesh", "Mesh", "Analysis mesh", GH_ParamAccess.item);
             pManager.AddGenericParameter("I", "I", "Results data from solar irradiation calculation.", GH_ParamAccess.item);
+            pManager.AddIntegerParameter("Value", "Value",
+                "Select the value to output: [0] = Total specific annual irradiation [kWh/m^2a], [1] = Specific beam annual [kWh/m^2a], [2] = Specific diffuse annual [kWh/m^2a], [3] = Total specific hourly [W/m^2], [4] = Specific beam hourly [W/m^2], [5] = Specific diffuse hourly [W/m^2], [6] = Solar Acces (defined as if beam radiation for at least 3 of the 4 vertices is not 0; outputs 1 if condition fulfilled, 0 otherwise).",
+                GH_ParamAccess.item);
+
         }
 
         /// <summary>
@@ -33,7 +37,7 @@ namespace GHSolar
         /// </summary>
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
-            pManager.AddNumberParameter("avgI", "avgI", "averaged hourly solar potentials in W/m^2", GH_ParamAccess.tree);
+            pManager.AddNumberParameter("avgI", "avgI", "averaged hourly solar irradiation or irradiance per mesh face as defined by 'Value' in the input.", GH_ParamAccess.tree);
             pManager.AddNumberParameter("area", "area", "area per mesh face in m^2", GH_ParamAccess.list);
         }
 
@@ -49,41 +53,70 @@ namespace GHSolar
             cResults results = null;
             if (!DA.GetData(1, ref results)) { return; }
 
+            int outputType = 0;
+            if (!DA.GetData(2, ref outputType)) { outputType = 0; }
 
-            //List<double[]> avgI = new List<double[]>();
+
             Grasshopper.DataTree<double> avgI = new Grasshopper.DataTree<double>();
-                
+
             List<double> areas = new List<double>();
 
             MeshFaceList mshfaces = mshin.Faces;
 
 
-            for(int i=0; i<mshfaces.Count; i++)
+            for (int m = 0; m < mshfaces.Count; m++)
             {
-                MeshFace fc = mshfaces[i];
+                MeshFace fc = mshfaces[m];
 
                 int spA = fc.A;
                 int spB = fc.B;
                 int spC = fc.C;
                 int spD = fc.D;
 
-                Grasshopper.Kernel.Data.GH_Path ghpath = new Grasshopper.Kernel.Data.GH_Path(i);
+                Grasshopper.Kernel.Data.GH_Path ghpath = new Grasshopper.Kernel.Data.GH_Path(m);
 
                 List<double> sp_I = new List<double>();
-                for (int t = 0; t < results.I_hourly.ColumnCount; t++)
+
+                switch (outputType)
                 {
-                    avgI.Add((results.I_hourly[spA, t] + results.I_hourly[spB, t] + results.I_hourly[spC, t] + results.I_hourly[spD, t]) / 4, ghpath);
-
-                    //sp_I.Add((results.I_hourly[spA, t] + results.I_hourly[spB, t] + results.I_hourly[spC, t] + results.I_hourly[spD, t]) / 4);
+                    case 0:
+                        avgI.Add((results.I_total[spA] + results.I_total[spB] + results.I_total[spC] + results.I_total[spD]) / 4, ghpath);
+                        break;
+                    case 1:
+                        avgI.Add((results.Ib_total[spA] + results.Ib_total[spB] + results.Ib_total[spC] + results.Ib_total[spD]) / 4, ghpath);
+                        break;
+                    case 2:
+                        avgI.Add((results.Id_total[spA] + results.Id_total[spB] + results.Id_total[spC] + results.Id_total[spD]) / 4, ghpath);
+                        break;
+                    case 3:
+                        for (int t = 0; t < results.I_hourly.ColumnCount; t++)
+                            avgI.Add((results.I_hourly[spA, t] + results.I_hourly[spB, t] + results.I_hourly[spC, t] + results.I_hourly[spD, t]) / 4, ghpath);
+                        break;
+                    case 4:
+                        for (int t = 0; t < results.Ib_hourly.ColumnCount; t++)
+                            avgI.Add((results.Ib_hourly[spA, t] + results.Ib_hourly[spB, t] + results.Ib_hourly[spC, t] + results.Ib_hourly[spD, t]) / 4, ghpath);
+                        break;
+                    case 5:
+                        for (int t = 0; t < results.Id_hourly.ColumnCount; t++)
+                            avgI.Add((results.Id_hourly[spA, t] + results.Id_hourly[spB, t] + results.Id_hourly[spC, t] + results.Id_hourly[spD, t]) / 4, ghpath);
+                        break;
+                    case 6:
+                        for (int t = 0; t < results.Ib_hourly.ColumnCount; t++)
+                        {
+                            int shdwcount = 0;
+                            if (results.Ib_hourly[spA, t] == 0) shdwcount++;
+                            if (results.Ib_hourly[spB, t] == 0) shdwcount++;
+                            if (results.Ib_hourly[spC, t] == 0) shdwcount++;
+                            if (results.Ib_hourly[spD, t] == 0) shdwcount++;
+                            if (shdwcount > 1) avgI.Add(0, ghpath);
+                            else avgI.Add(1, ghpath);
+                        }
+                        break;
                 }
-
-               
-
-                areas.Add(cMisc.getMeshFaceArea(i, mshin));
-
+                areas.Add(cMisc.getMeshFaceArea(m, mshin));
             }
 
-            
+
 
             DA.SetDataTree(0, avgI);
             DA.SetDataList(1, areas);
